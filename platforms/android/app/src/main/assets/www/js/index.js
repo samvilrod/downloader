c6904f5d9568c3a7d0cf28b7f05b9546ad7e098e
samvilrod <samuel_villa_rdz@outlook.com>
2019-11-28 14:15:06 -0600

    downloader descarga de zip

--- platforms/android/app/src/main/assets/www/js/index.js
+++ platforms/android/app/src/main/assets/www/js/index.js
@@ -4,16 +4,24 @@ document.addEventListener('deviceready', function(){
     //    for example, cdvfile://localhost/persistent/path/to/downloads/
 
     var fileTransfer = new FileTransfer();
-    var uri = encodeURI("https://oneclicksoftwaresolutions.com/assets/meme.png");//lo que queremos descargar
+    var uri = encodeURI("https://fantastikapps.com/puiki/x.zip");//lo que queremos descargar
     // https://oneclicksoftwaresolutions.com/assets/meme.png
     var androidVersion = parseInt(device.version);
-    var dataPath = (androidVersion == 5) ? 'file:///storage/sdcard0/Android/data/' : 'file:///storage/emulated/0/Android/data/';
+    var dataPath = (androidVersion == 9) ? 'file:///storage/sdcard0/Android/data/' : 'file:///storage/emulated/0/Android/data/';
     fileTransfer.download(
         uri,
-        dataPath + '/prueba.png',
+        dataPath + '/prueba.zip',
         function(entry) {
             alert("success");
             console.log("download complete: " + entry.toURL());
+            // var PathToFileInString  = cordova.file.externalRootDirectory+"HereIsMyFile.zip",
+            //     PathToResultZip     = cordova.file.externalRootDirectory;
+            // JJzip.unzip(PathToFileInString, {target:PathToResultZip},function(data){
+            //     /* Wow everything goes good, but just in case verify data.success */
+            // },function(error){
+            //     /* Wow something goes wrong, check the error.message */
+            // })
+
             var img = document.createElement("img");
             img.src =  entry.toURL();
             document.body.appendChild(img);
@@ -26,4 +34,6 @@ document.addEventListener('deviceready', function(){
             console.log("download error code" + error.code);
         }
     );
+
+
 }, false);
